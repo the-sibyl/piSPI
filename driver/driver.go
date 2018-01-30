67061514257ab63017a9d98473a8a204b81a2fb7
User U. User <user@user.user>
2018-01-30 03:32:45 +0000

    Adding a tarball of the original code. Removing the comments on the same line as the package names to de-canonicalize the package. I may come back and rename things later.

--- driver/driver.go
+++ driver/driver.go
@@ -3,7 +3,7 @@
 // license that can be found in the LICENSE file.
 
 // Package driver contains interfaces to be implemented by various SPI implementations.
-package driver // import "golang.org/x/exp/io/spi/driver"
+package driver
 
 const (
 	Mode = iota
